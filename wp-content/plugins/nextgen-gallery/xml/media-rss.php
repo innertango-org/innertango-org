39d852b8890218944f6c907da4589ecad79d4f86
polcom <paulxinas@gmail.com>
2019-05-03 01:27:00 +0300

    first commit

---
+++ wp-content/plugins/nextgen-gallery/xml/media-rss.php
@@ -0,0 +1,87 @@
+<?xml version='1.0' encoding='UTF-8' standalone='yes'?>
+<rss version='2.0' xmlns:media='http://search.yahoo.com/mrss' xmlns:atom='http://www.w3.org/2005/Atom'>
+	<channel>
+		<generator><![CDATA[NextGEN Gallery [http://alexrabe.boelinger.com]]]></generator>
+		<title></title>
+		<description>The healing power of a mythical dance</description>
+		<link><![CDATA[http://innertango.org]]></link>
+		<atom:link rel='next' href='http://innertango.org/wp-content/plugins/nextgen-gallery/xml/media-rss.php?show=10&amp;page=1&amp;mode=last_pictures' />
+		<item>
+			<title><![CDATA[workshop-xanthi2]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://innertango.org/wp-content/gallery/workshop-photos/workshop-xanthi2.jpg]]></link>
+			<media:content url='http://innertango.org/wp-content/gallery/workshop-photos/workshop-xanthi2.jpg' medium='image' />
+			<media:title><![CDATA[workshop-xanthi2]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://innertango.org/wp-content/gallery/workshop-photos/thumbs/thumbs_workshop-xanthi2.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[tango workshop, xanthi]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c)  (http://innertango.org)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[workshop-xanthi1]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://innertango.org/wp-content/gallery/workshop-photos/workshop-xanthi1.jpg]]></link>
+			<media:content url='http://innertango.org/wp-content/gallery/workshop-photos/workshop-xanthi1.jpg' medium='image' />
+			<media:title><![CDATA[workshop-xanthi1]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://innertango.org/wp-content/gallery/workshop-photos/thumbs/thumbs_workshop-xanthi1.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[tango workshop, xanthi]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c)  (http://innertango.org)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[workshop-xanthi-4]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://innertango.org/wp-content/gallery/workshop-photos/workshop-xanthi-4.jpg]]></link>
+			<media:content url='http://innertango.org/wp-content/gallery/workshop-photos/workshop-xanthi-4.jpg' medium='image' />
+			<media:title><![CDATA[workshop-xanthi-4]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://innertango.org/wp-content/gallery/workshop-photos/thumbs/thumbs_workshop-xanthi-4.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[tango workshop, xanthi]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c)  (http://innertango.org)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[workshop-xanthi-3]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://innertango.org/wp-content/gallery/workshop-photos/workshop-xanthi-3.jpg]]></link>
+			<media:content url='http://innertango.org/wp-content/gallery/workshop-photos/workshop-xanthi-3.jpg' medium='image' />
+			<media:title><![CDATA[workshop-xanthi-3]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://innertango.org/wp-content/gallery/workshop-photos/thumbs/thumbs_workshop-xanthi-3.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[tango workshop, xanthi]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c)  (http://innertango.org)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[athens-group3]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://innertango.org/wp-content/gallery/workshop-photos/athens-group3.jpg]]></link>
+			<media:content url='http://innertango.org/wp-content/gallery/workshop-photos/athens-group3.jpg' medium='image' />
+			<media:title><![CDATA[athens-group3]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://innertango.org/wp-content/gallery/workshop-photos/thumbs/thumbs_athens-group3.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[tango workshop, athens]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c)  (http://innertango.org)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[athens-group2]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://innertango.org/wp-content/gallery/workshop-photos/athens-group2.jpg]]></link>
+			<media:content url='http://innertango.org/wp-content/gallery/workshop-photos/athens-group2.jpg' medium='image' />
+			<media:title><![CDATA[athens-group2]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://innertango.org/wp-content/gallery/workshop-photos/thumbs/thumbs_athens-group2.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[tango workshop, athens]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c)  (http://innertango.org)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[athens-group1]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://innertango.org/wp-content/gallery/workshop-photos/athens-group1.jpg]]></link>
+			<media:content url='http://innertango.org/wp-content/gallery/workshop-photos/athens-group1.jpg' medium='image' />
+			<media:title><![CDATA[athens-group1]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://innertango.org/wp-content/gallery/workshop-photos/thumbs/thumbs_athens-group1.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[tango workshop, athens]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c)  (http://innertango.org)]]></media:copyright>
+		</item>
+	</channel>
+</rss>
